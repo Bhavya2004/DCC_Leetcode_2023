0214ef5f989fceedcf4b3153fecfd166933589e6
Bhavya Barai <94623439+Bhavya2004@users.noreply.github.com>
2023-02-01 13:08:34 +0530

    Add files via upload

---
+++ February 2023/greatest_common_divisor.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        // Check if concatenated strings are equal or not, if not return ""
+        if (!(str1 + str2).equals(str2 + str1))
+            return "";
+        // If strings are equal than return the substring from 0 to gcd of size(str1), size(str2)
+        int gcd = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcd);
+    }
+
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+}
