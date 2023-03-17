2ee3abf9995a4e7deddb6bd950af77eab00083fa
Bhavya Barai <94623439+Bhavya2004@users.noreply.github.com>
2023-03-17 10:06:20 +0530

    Add files via upload

---
+++ March 2023/Implement_Trie_Prefix_Tree.py
@@ -0,0 +1,36 @@
+class Trie:
+    def __init__(self):
+        self.root = {}
+        
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node:
+                node[char] = {}
+            node = node[char]
+        node['$'] = True
+        
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node:
+                return False
+            node = node[char]
+        return '$' in node
+        
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node:
+                return False
+            node = node[char]
+        return True
+
+        
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
