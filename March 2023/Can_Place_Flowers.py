9e6152f02648332aa0dc2f05721b11a02110e616
Bhavya Barai <94623439+Bhavya2004@users.noreply.github.com>
2023-03-20 09:21:16 +0530

    Add files via upload

---
+++ March 2023/Can_Place_Flowers.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        count = 0
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0 and (i == 0 or flowerbed[i-1] == 0) and (i == len(flowerbed)-1 or flowerbed[i+1] == 0):
+                flowerbed[i] = 1
+                count += 1
+                if count >= n:
+                    return True
+        return count >= n
+
+        
