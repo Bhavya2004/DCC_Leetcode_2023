e33721141823722dbb457c3308f14b0d28e1f00f
Bhavya Barai <94623439+Bhavya2004@users.noreply.github.com>
2023-06-03 11:59:02 +0530

    Add files via upload

---
+++ June 2023/Time_Needed_to_Inform_All_Employees.py
@@ -0,0 +1,16 @@
+class Solution:
+
+    def dfs(self,manager,informTime,adjList):
+        maxTime=0
+        for subordinate in adjList[manager]:
+            maxTime=max(maxTime,self.dfs(subordinate,informTime,adjList))
+        return maxTime + informTime[manager]
+
+
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+
+        adjList = defaultdict(list)
+        for i in range(n):
+            if manager[i] != -1:
+                adjList[manager[i]].append(i)
+        return self.dfs(headID,informTime,adjList)
