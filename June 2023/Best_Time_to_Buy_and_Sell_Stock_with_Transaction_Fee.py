2175f5fb5d542995e02c19800afbc1e137b7976c
Bhavya Barai <94623439+Bhavya2004@users.noreply.github.com>
2023-06-22 18:06:41 +0530

    Add files via upload

---
+++ June 2023/Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        n = len(prices)
+        if n < 2:
+             return 0
+        ans = 0
+        minimum = prices[0]
+        for i in range(1, n):
+            if prices[i] < minimum:
+                minimum = prices[i]
+            elif prices[i] > minimum + fee:
+                ans += prices[i] - fee - minimum
+                minimum = prices[i] - fee
+        return ans
