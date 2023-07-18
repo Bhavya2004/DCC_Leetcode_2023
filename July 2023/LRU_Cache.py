50fdc35689c824462ce0cb9ca178c66d1eeea3c8
Bhavya Barai <94623439+Bhavya2004@users.noreply.github.com>
2023-07-18 17:41:02 +0530

    Add files via upload

---
+++ July 2023/LRU_Cache.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.head = Node(None, None)  # dummy head node
+        self.tail = Node(None, None)  # dummy tail node
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove_node(node)
+            self._add_to_head(node)
+            return node.value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            node = self.cache[key]
+            node.value = value
+            self._remove_node(node)
+            self._add_to_head(node)
+        else:
+            if len(self.cache) >= self.capacity:
+                self._remove_tail()
+            new_node = Node(key, value)
+            self.cache[key] = new_node
+            self._add_to_head(new_node)
+
+    def _remove_node(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def _add_to_head(self, node):
+        node.prev = self.head
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next = node
+
+    def _remove_tail(self):
+        tail_node = self.tail.prev
+        del self.cache[tail_node.key]
+        self._remove_node(tail_node)
