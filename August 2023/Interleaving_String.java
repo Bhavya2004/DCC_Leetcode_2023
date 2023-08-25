26a941c7e607c014db16f5d38adfcd68d4b5e338
Bhavya Barai <94623439+Bhavya2004@users.noreply.github.com>
2023-08-25 19:23:16 +0530

    Add files via upload

---
+++ August 2023/Interleaving_String.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int m = s1.length(), n = s2.length(), l = s3.length();
+        if (m + n != l) return false;
+
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+
+        for (int j = 1; j <= n; ++j) {
+            dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
+        }
+
+        for (int i = 1; i <= m; ++i) {
+            dp[0] = dp[0] && s1.charAt(i - 1) == s3.charAt(i - 1);
+            for (int j = 1; j <= n; ++j) {
+                dp[j] = (dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) || (dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+            }
+        }
+        
+        return dp[n];
+    }
+}
