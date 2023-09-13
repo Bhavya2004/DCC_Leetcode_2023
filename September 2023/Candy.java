57260a5de6d17e5df3973f4ca0b426fd8bf8e5de
Bhavya Barai <94623439+Bhavya2004@users.noreply.github.com>
2023-09-13 11:32:04 +0530

    Add files via upload

---
+++ September 2023/Candy.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candies = new int[n];
+        
+        // Initialize each child with 1 candy
+        Arrays.fill(candies, 1);
+        
+        if (n == 1) {
+            return 1;
+        }
+        
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+        
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
+                candies[i] = candies[i + 1] + 1;
+            }
+        }
+        
+        int totalCandies = 0;
+        for (int i = 0; i < n; i++) {
+            totalCandies += candies[i];
+        }
+        
+        return totalCandies;
+    }
+}
