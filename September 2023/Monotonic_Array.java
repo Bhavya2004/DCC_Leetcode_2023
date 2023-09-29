a8ecf412bfdc6c573bb6c7dea5ed8319dd0eb8af
Bhavya Barai <94623439+Bhavya2004@users.noreply.github.com>
2023-09-29 19:27:36 +0530

    Add files via upload

---
+++ September 2023/Monotonic_Array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        boolean increasing = true;
+        boolean decreasing = true;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                decreasing = false;
+            } else if (nums[i] < nums[i - 1]) {
+                increasing = false;
+            }
+        }
+
+        return increasing || decreasing;
+    }
+}
