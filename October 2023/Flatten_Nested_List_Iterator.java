511f48f001a576d1563ebed80e646ce18811ac15
Bhavya Barai <94623439+Bhavya2004@users.noreply.github.com>
2023-10-20 11:55:09 +0530

    Add files via upload

---
+++ October 2023/Flatten_Nested_List_Iterator.java
@@ -0,0 +1,31 @@
+public class NestedIterator implements Iterator<Integer> {
+    private Stack<NestedInteger> stack;
+
+    public NestedIterator(List<NestedInteger> nestedList) {
+        stack = new Stack<>();
+        flattenList(nestedList);
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            return null;
+        }
+        return stack.pop().getInteger();
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (!stack.isEmpty() && !stack.peek().isInteger()) {
+            List<NestedInteger> nestedList = stack.pop().getList();
+            flattenList(nestedList);
+        }
+        return !stack.isEmpty();
+    }
+
+    private void flattenList(List<NestedInteger> nestedList) {
+        for (int i = nestedList.size() - 1; i >= 0; i--) {
+            stack.push(nestedList.get(i));
+        }
+    }
+}
