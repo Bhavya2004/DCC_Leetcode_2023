8ba5d091541f3af08e6ed866ccce168b88252702
Bhavya Barai <94623439+Bhavya2004@users.noreply.github.com>
2023-12-09 14:52:25 +0530

    Add files via upload

---
+++ December 2023/Binary_Tree_Inorder_Traversal.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorderHelper(root, result);
+        return result;
+    }
+    private void inorderHelper(TreeNode node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+        inorderHelper(node.left, result);
+        result.add(node.val);
+        inorderHelper(node.right, result);
+    }
+}
